a87c84e3a08119e9a35a430c6d6a218c7c1ab3a4
Eryk1997 <erykmati@o2.pl>
2020-02-21 14:51:55 +0100

    arithmetic operators

---
+++ 8 What are arithmetic operators in php/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Arithmetic operators
+        echo "5+5= ";
+        echo 5+5 . "</br>";
+
+        echo "10-2= ";
+        echo 10-2 . "</br>";
+
+        echo "5%5= ";
+        echo 5%5 . "</br>";
+
+        echo "5*5= ";
+        echo 5*5 . "</br>";
+
+        echo "5/2= ";
+        echo 5/2 . "</br>";
+
+        echo "5**5= ";
+        echo 5**5;
+    ?>
+
+</body>
+</html>
