792ec5dbd343a0e130f4f2fae3fea2baa35012ec
Eryk1997 <erykmati@o2.pl>
2020-02-22 12:37:12 +0100

    logical operators

---
+++ 12 What are logical operators in php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 10;
+        $y = 20;
+
+        if ($x == $y || 1 == 1) {
+            echo "True";
+        }
+    ?>
+</body>
+</html>
