60df332402fc44fc17c550f10735a8384a545789
Eryk1997 <erykmati@o2.pl>
2020-02-22 12:26:18 +0100

    assignment operators in php

---
+++ 9 what are assignment operators in php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $x = 100;
+        $x = $x + 20;
+        $x += 20;
+
+        echo $x;
+    ?>
+</body>
+</html>
