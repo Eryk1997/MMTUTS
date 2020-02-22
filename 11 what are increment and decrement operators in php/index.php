9efd18a32679aea7e68abae182d07f7a7961b38f
Eryk1997 <erykmati@o2.pl>
2020-02-22 12:36:27 +0100

    increment and decrement

---
+++ 11 what are increment and decrement operators in php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 10;
+        echo ++$x . "</br>";
+
+        echo $x++ . "</br>";
+
+        echo $x;
+
+    ?>
+</body>
+</html>
