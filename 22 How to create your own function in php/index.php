2cbf122a3e7596e69c74d3e04bbb97b137ed20eb
Eryk1997 <erykmati@o2.pl>
2020-02-25 20:51:55 +0100

    strong

--- 22 How to create your own function in php/index.php
+++ 22 How to create your own function in php/index.php
@@ -9,13 +9,34 @@
 
         function newCalc($x){
             $newnr = $x * 0.75;
-            echo "Here is 75% of what you wrote: " . $newnr;
+            echo "Here is 75% of what you wrote: " . $newnr . "<br>";
+        }
+
+        function strong($value){
+            $result = 1;
+            if($value == 1 || $value == 0){
+                $result = 1;
+            }
+            elseif ($value == 2) {
+                $result = 2;
+            }
+            else{
+                for ($i=2; $i <= $value; $i++) {
+                    $result *= $i;
+                }
+            }
+            echo $result . "<br>";
         }
 
         $x = 100;
 
         newCalc($x);
-
+        strong(2);
+        strong(3);
+        strong(4);
+        strong(5);
+        strong(6);
+        strong(7);
     ?>
 
 
