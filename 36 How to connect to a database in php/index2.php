e070e4c70df8d276da386d758dab7cb0a7baa43a
Eryk1997 <erykmati@o2.pl>
2020-03-01 19:08:55 +0100

    prepared statements

---
+++ 36 How to connect to a database in php/index2.php
@@ -0,0 +1,40 @@
+<?php
+    include_once 'includes/connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    $data = "Admin";
+    //created a template
+    $sql = "SELECT * FROM users where user_uid=?;";
+    //create a prepared statement
+    $stmt = mysqli_stmt_init($conn);
+    //prepare the prepared statement
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "SQL statement failed";
+    } else{
+        //Bind parameters to the placeholder
+        mysqli_stmt_bind_param($stmt, "s", $data);
+        //RUN parameters inside database
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['user_uid'] . "<br>";
+        }
+    }
+
+
+
+?>
+
+
+</body>
+</html>
