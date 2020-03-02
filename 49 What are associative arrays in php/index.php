faaf509d0e8ab63d06412152f439c33f06cd3769
Eryk1997 <erykmati@o2.pl>
2020-03-02 11:04:47 +0100

    associative arrays

---
+++ 49 What are associative arrays in php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $data = array(
+            "first" => "Daniel",
+            'last' => "Nielsen",
+            'age' => 25);
+        echo $data['first'];
+    ?>
+
+</body>
+</html>
