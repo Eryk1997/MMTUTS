1d4f96737c64392bc2cdb98f7435991da4fd0c82
Eryk1997 <erykmati@o2.pl>
2020-03-02 09:53:47 +0100

    array

---
+++ 45 What are arrays used for in php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$person = "Daniel";
+$people = array("Daniel", "Jane", "John");
+echo $people[0];
+echo "<br>";
+foreach ($people as $x) echo $x . "<br>";
+
+?>
+</body>
+</html>
