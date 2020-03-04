406aacee11759dc2da14cf0e8dde6980534ce4b4
Eryk1997 <erykmati@o2.pl>
2020-03-04 12:56:27 +0100

    Delete more than one file in folder

--- 54: How to delete files from folder/deletefile.php
+++ 54: How to delete files from folder/deletefile.php
@@ -1,10 +1,26 @@
 <?php
 
-$path = "eryk.jpg";
+$fileNames = $_POST['fileName'];
+$removeSpaces = str_replace(" ", "",$fileNames);
+$allFileNames = explode(",",$removeSpaces);
+$countAllNames = count($allFileNames);
 
-if (!unlink($path)) {
-    echo "You have an error!";
-}else{
-    echo "You have deleted your file!";
-    header("Location: index.php?deletesuccess");
+for ($i=0; $i < $countAllNames; $i++) {
+    if (file_exists("uploads/" . $allFileNames[$i]) == false) {
+        header("Location: index.php?deleteerror");
+        exit();
+    }
 }
+
+for ($i=0; $i < $countAllNames; $i++) {
+    $path = "uploads/" . $allFileNames[$i];
+    if (!unlink($path)) {
+        echo "You have an error!";
+        exit();
+    }
+}
+
+header("Location: index.php?deletesuccess");
+
+
+
--- 54: How to delete files from folder/index.php
+++ 54: How to delete files from folder/index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
     <form action="deletefile.php" method="POST">
+        <input type="text" name="fileName" placeholder="Separate each name with a comma (,)" style="width: 300px">
         <button type="submit" name="submit">Delete fie</button>
     </form>
 
