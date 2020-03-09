cffc4c0cdf1b545af51e726d536a5e16c54c8c17
Eryk1997 <erykmati@o2.pl>
2020-03-09 17:55:31 +0100

    resolution operators

---
+++ Object Oriented PHP/12 Scope resolution operators in oop php/classes/firstclass.cla.php
@@ -0,0 +1,12 @@
+<?php
+
+class FirstClass{
+
+     const EXAMPLE = "You can't change this! ";
+
+     public static function test(){
+         $testing = "This is a test";
+         return $testing;
+     }
+}
+
